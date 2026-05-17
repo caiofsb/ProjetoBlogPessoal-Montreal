8d89b321de1764865a1b66a807d3d1a661a46d65
Caio Felipe <caio.bindeiro@gmail.com>
2026-05-17 10:37:46 -0300

    Registrando o  DbContext e configurando a conexão com postgress

--- BlogPessoal/Program.cs
+++ BlogPessoal/Program.cs
@@ -1,9 +1,18 @@
 using BlogPessoal.Config;
+using BlogPessoal.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerConfiguration();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
@@ -19,7 +28,7 @@ app.MapGet("/api/health", () =>
 {
     return Results.Ok(new
     {
-        status = "API do Blog Pessoal funcionando",
+        status = "API do Blog Pessoal está funcionando",
         ambiente = app.Environment.EnvironmentName,
         data = DateTime.UtcNow
     });
