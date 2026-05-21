fe31e20e3588fe50df1ba970b46e6a812c5e5b59
Caio Felipe <caio.bindeiro@gmail.com>
2026-05-21 15:18:23 -0300

    Adiciona projeto pra começar testes unitários

---
+++ BlogPessoal.Tests/AmbienteTests.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogPessoal.Tests;
+
+public class AmbienteTests
+{
+    [Fact]
+    public void DeveExecutarTesteBasico()
+    {
+        var resultado = true;
+
+        Assert.True(resultado);
+    }
+}
